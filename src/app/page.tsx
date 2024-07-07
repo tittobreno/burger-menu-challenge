6b7bf99445de04c199f5ea716ad3eaf261b7f158
Breno <brenomoraes97@yahoo.com>
2024-07-07 02:03:36 -0300

    feat(input): created input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { restaurantServices } from "@/services/restaurant";
+import { Search } from "lucide-react";
 import Image from "next/image";
 import { useEffect } from "react";
 
@@ -29,6 +30,22 @@ export default function Home() {
           layout="fill"
           objectFit="cover"
         />
+      </section>
+
+      <section className="w-full flex flex-col items-center justify-center">
+        <section className="w-[1024px]">
+          <div className="w-full bg-white border border-gray-400 rounded-lg flex items-center p-3 gap-3 my-[6px]">
+            <Search className="text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search menu items"
+              className="w-full border-none bg-transparent shadow-none outline-none focus:outline-none focus:ring-0 p-0 m-0"
+            />
+          </div>
+          <section className="bg-gray-100 w-full">
+            <span>asdas</span>
+          </section>
+        </section>
       </section>
     </main>
   );
